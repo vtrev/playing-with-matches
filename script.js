f8a8444df8940ec4e1e97dc0857794d6bdd22805
vusi <baloyi.trev@gmail.com>
2018-08-17 15:59:15 +0200

    the matching numbers project changing colors

--- script.js
+++ script.js
@@ -1,28 +1,87 @@
 //    grtting the div to place the random number
+let randomNumberSelector = document.querySelectorAll('.number');
+// let randomNumberKey = Object.keys(randomNumbers);
+
+
+var randomNumbers = {
+    num1: {
+        number: 0,
+        state: ''
+    },
+    num2: {
+        number: 0,
+        state: ''
+    },
+    num3: {
+        number: 0,
+        state: ''
+    }
+};
+let randomNumberKey = Object.keys(randomNumbers);
+
+
+
 
 // function that creates a random number between 1 and 9
-
 function makeRandomNumber() {
     let randomNumber = Math.floor(Math.random() * 9) + 1;
     return randomNumber
-
 };
 
 // function that returns the random number
 
 function displayRandomNumbers() {
-    let num1 = makeRandomNumber();
-    let num2 = makeRandomNumber();
-    let num3 = makeRandomNumber();
 
-    let randomNumberSelector = document.querySelectorAll('.number');
+    for (i = 0; i < randomNumberSelector.length; i++) {
+        let tmpRandomNumber = makeRandomNumber();
+        let num = randomNumberKey[i];
+        randomNumbers[num].number = tmpRandomNumber;
+        randomNumberSelector[i].innerHTML = tmpRandomNumber;
+    };
+
+}
+
+// find matches
+
+function findMatch(numbersoMatch) {
+    if (numbersoMatch.num1.number == numbersoMatch.num2.number) {
+        numbersoMatch.num1.state = 'match';
+        numbersoMatch.num2.state = 'match';
+        messageBox.innerHTML = 'Matching numbers : ' + numbersoMatch.num1.number;
 
 
-    randomNumberSelector[0].innerHTML = num1;
-    randomNumberSelector[1].innerHTML = num2;
-    randomNumberSelector[2].innerHTML = num3;
+    } else if (numbersoMatch.num2.number == numbersoMatch.num3.number) {
+        numbersoMatch.num2.state = 'match';
+        numbersoMatch.num3.state = 'match';
+        messageBox.innerHTML = 'Matching numbers : ' + numbersoMatch.num3.number;
+
+    } else if (numbersoMatch.num1.number == numbersoMatch.num3.number) {
+        numbersoMatch.num1.state = 'match';
+        numbersoMatch.num3.state = 'match';
+        messageBox.innerHTML = 'Matching numbers : ' + numbersoMatch.num1.number;
+
+    } else if (numbersoMatch.num1.number == numbersoMatch.num2.number && numbersoMatch.num1.number == numbersoMatch.num3.number) {
+        numbersoMatch.num1.state = 'match';
+        numbersoMatch.num2.state = 'match';
+        numbersoMatch.num3.state = 'match';
+        messageBox.innerHTML = 'Matching numbers : ' + num1;
+    };
+    addClass(randomNumbers);
+
 };
 
-// call the displayRandomNumberFunction
 
+
+function addClass(numbers) {
+    for (i = 0; i < randomNumberKey.length; i++) {
+        let num = randomNumberKey[i];
+        console.log(numbers[num].state);
+        if (numbers[num].state == 'match') {
+            randomNumberSelector[i].classList.add('match');
+        }
+    }
+
+
+}
 displayRandomNumbers();
+findMatch(randomNumbers);
